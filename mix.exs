38ac9f9db16d539cfa1c9e411dcbfd04a8426ef8
Joshua Rieken <joshua@joshuarieken.com>
2016-01-07 15:59:03 -0700

    Initial mix.exs

--- mix.exs
+++ mix.exs
@@ -5,12 +5,16 @@ defmodule Plasm.Mixfile do
   @adapters [:pg, :mysql]
 
   def project do
-    [app: :plasm,
-     version: @version,
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :plasm,
+      version: @version,
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      description: description,
+      package: package,
+    ]
   end
 
   # Configuration for the OTP application
@@ -30,6 +34,24 @@ defmodule Plasm.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ecto, ""}]
+    [{:ecto, "~> 1.0"},
+     {:ex_doc, "~> 0.10", only: :docs},
+     {:earmark, "~> 0.1", only: :docs},
+     {:inch_ex, only: :docs}]
+  end
+
+  defp description do
+    """
+    Plasm is a composable query library for Ecto containing several common query transforms to make working with Ecto easier.
+    """
+  end
+
+  defp package do
+    [
+      maintainers: ["Jason Harrelson", "Joshua Rieken"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/atomic-fads/plasm"},
+      files: ~w(mix.exs README.md CHANGELOG.md lib)
+    ]
   end
 end
