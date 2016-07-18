a1425e674202d3bd602b0c8dfd41683e7f1bc44b
Joshua Rieken <joshua@joshuarieken.com>
2016-07-18 15:32:09 -0700

    Alignment

--- mix.exs
+++ mix.exs
@@ -5,17 +5,17 @@ defmodule Plasm.Mixfile do
 
   def project do
     [
-      app: :plasm,
-      version: @version,
-      elixir: "~> 1.0",
-      elixirc_paths: elixirc_paths(Mix.env),
-      build_embedded: Mix.env == :prod,
+      app:             :plasm,
+      version:         @version,
+      elixir:          "~> 1.0",
+      elixirc_paths:   elixirc_paths(Mix.env),
+      build_embedded:  Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps,
-      description: description,
-      package: package,
-      aliases: aliases,
-      docs: [extras: ["README.md", "CHANGELOG.md"]],
+      deps:            deps,
+      description:     description,
+      package:         package,
+      aliases:         aliases,
+      docs:            [extras: ["README.md", "CHANGELOG.md"]],
     ]
   end
 
