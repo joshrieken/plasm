2f315674ece46563867ab6582ee5a21fddbfdf52
Joshua Rieken <joshua@joshuarieken.com>
2016-07-18 20:25:03 -0700

    Add spec for `avg` w/ String.t field name

--- test/api/avg_test.exs
+++ test/api/avg_test.exs
@@ -1,10 +1,26 @@
 defmodule Plasm.Api.AvgTest do
   use Plasm.ApiCase
 
-  test ".avg" do
+  test ".avg with an atom field name" do
     plasm_query_string =
       Plasm.User
       |> Plasm.avg(:age)
+      |> query_to_string
+
+    ecto_query_string =
+      Ecto.Query.from(
+        u in Plasm.User,
+        select: avg(u.age)
+      )
+      |> query_to_string
+
+    assert plasm_query_string == ecto_query_string
+  end
+
+  test ".avg with a String.t field name" do
+    plasm_query_string =
+      Plasm.User
+      |> Plasm.avg("age")
       |> query_to_string
 
     ecto_query_string =
