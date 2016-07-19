73f1af68df4ce377a337bc11a62930867d10c9db
Joshua Rieken <joshua@joshuarieken.com>
2016-07-19 11:12:58 -0700

    Add tests for min and max String.t field names

--- test/api/max_test.exs
+++ test/api/max_test.exs
@@ -1,10 +1,26 @@
 defmodule Plasm.Api.MaxTest do
   use Plasm.ApiCase
 
-  test ".max" do
+  test ".max with an atom field name" do
     plasm_query_string =
       Plasm.User
       |> Plasm.max(:age)
+      |> query_to_string
+
+    ecto_query_string =
+      Ecto.Query.from(
+        u in Plasm.User,
+        select: max(u.age)
+      )
+      |> query_to_string
+
+    assert plasm_query_string == ecto_query_string
+  end
+
+  test ".max with a String.t field name" do
+    plasm_query_string =
+      Plasm.User
+      |> Plasm.max("age")
       |> query_to_string
 
     ecto_query_string =
--- test/api/min_test.exs
+++ test/api/min_test.exs
@@ -1,10 +1,26 @@
 defmodule Plasm.Api.MinTest do
   use Plasm.ApiCase
 
-  test ".min" do
+  test ".min with an atom field name" do
     plasm_query_string =
       Plasm.User
       |> Plasm.min(:age)
+      |> query_to_string
+
+    ecto_query_string =
+      Ecto.Query.from(
+        u in Plasm.User,
+        select: min(u.age)
+      )
+      |> query_to_string
+
+    assert plasm_query_string == ecto_query_string
+  end
+
+  test ".min with a String.t field name" do
+    plasm_query_string =
+      Plasm.User
+      |> Plasm.min("age")
       |> query_to_string
 
     ecto_query_string =
