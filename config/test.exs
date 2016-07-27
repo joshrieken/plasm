4cc4d005b4ae3b02a6e8904ca0dd9f6d026794e5
Joshua Rieken <joshua@joshuarieken.com>
2016-07-27 10:10:52 -0700

    Configure postgres for travis

--- config/test.exs
+++ config/test.exs
@@ -4,6 +4,6 @@ config :plasm, Plasm.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   database: "plasm_test",
-  username: System.get_env("PLASM_DB_USER") || System.get_env("USER")
+  username: "postgres"
 
 config :logger, level: :info
