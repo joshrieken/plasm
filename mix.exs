2bbc8dfac8f5c490000b3d80fb03636b41c97c94
Joshua Rieken <joshua@joshuarieken.com>
2020-06-09 11:37:18 -0600

    Do not load test assets in non-test envs

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule Plasm.Mixfile do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   defp aliases do
     [
